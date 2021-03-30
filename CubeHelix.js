4fa266f60213800693ae9cf2c90bbf93a634fcc6
kjosborn <81536999+kjosborn@users.noreply.github.com>
2021-03-30 16:25:54 +1100

    added numeric control include

--- CubeHelix.js
+++ CubeHelix.js
@@ -14,6 +14,7 @@
                 (https://astron-soc.in/bulletin/11June/289392011.pdf). 
 
 #include <pjsr/Sizer.jsh>
+#include <pjsr/NumericControl.jsh>
 #include <pjsr/TextAlign.jsh>
 #include <pjsr/UndoFlag.jsh>
 #include <pjsr/FrameStyle.jsh>
