fa78c9ad6d538d1c83364d39e45e25ad1f88b60a
kjosborn <81536999+kjosborn@users.noreply.github.com>
2021-04-02 16:20:38 +1100

    Script can open without active window

--- CubeHelix.js
+++ CubeHelix.js
@@ -7,11 +7,11 @@
  * (C) Kevin Osborn 2021
  */
 
- #feature-id    Utilities > CubeHelix
+#feature-id    Utilities > CubeHelix
  
- #feature-info  Script to display mono image as mapped pseudo colour\
-                using Dave Green's CubeHelix algorithm \
-                (https://astron-soc.in/bulletin/11June/289392011.pdf). 
+#feature-info  Script to display mono image as mapped pseudo colour\
+               using Dave Green's CubeHelix algorithm \
+               (https://astron-soc.in/bulletin/11June/289392011.pdf).
 
 #include <pjsr/Sizer.jsh>
 #include <pjsr/NumericControl.jsh>
@@ -31,8 +31,7 @@
 
 // #define PREVIEW_SIZE 400
 
-function CubeHelixColourData()
-{
+function CubeHelixColourData() {
     this.startColour = 0.0;
     this.rotations = 0.0;
     this.saturation = 0.0;
@@ -41,24 +40,37 @@ function CubeHelixColourData()
     this.colourScale = null;
     this.sourceView = null;
 
-    this.sourceView = ImageWindow.activeWindow.mainView;
-    if (this.sourceView.isNull) {
-        var msg = new MessageBox("Source image view must be selected", "CubeHelix Error", StdIcon_Error, StdButton_Ok);
-        console.hide();
-        msg.execute();
-        throw new Error("Source image must be selected.");
+    // this.sourceView = ImageWindow.activeWindow.mainView;
+    // if (this.sourceView.isNull) {
+    //     var msg = new MessageBox("Source image view must be selected", "CubeHelix Error", StdIcon_Error, StdButton_Ok);
+    //     console.hide();
+    //     msg.execute();
+    //     throw new Error("Source image must be selected.");
+    // }
+
+    // this.preview = new ImageWindow(this.sourceView.image.width, this.sourceView.image.height,
+    //     3, 32, true, true, "CubeHelix");
+    // this.preview.mainView.beginProcess(UndoFlag_NoSwapFile);
+    // this.preview.mainView.image.fill(0);
+    // this.preview.mainView.endProcess();
+    // this.preview.fitWindow();
+    // this.preview.zoomToOptimalFit();
+    // this.preview.show();
+
+    this.initPreview = function (sourceImg) {
+        this.sourceView = sourceImg;
+        this.preview = new ImageWindow(this.sourceView.image.width, this.sourceView.image.height,
+            3, 32, true, true, "CubeHelix");
+        this.preview.mainView.beginProcess(UndoFlag_NoSwapFile);
+        this.preview.mainView.image.fill(0);
+        this.preview.mainView.endProcess();
+        this.preview.fitWindow();
+        this.preview.zoomToOptimalFit();
+        this.preview.show();
+
     }
 
-    this.preview = new ImageWindow(this.sourceView.image.width, this.sourceView.image.height,
-        3, 32, true, true, "CubeHelix");
-    this.preview.mainView.beginProcess(UndoFlag_NoSwapFile);
-    this.preview.mainView.image.fill(0);
-    this.preview.mainView.endProcess();
-    this.preview.fitWindow();
-    this.preview.zoomToOptimalFit();
-    this.preview.show();
-
-    this.updateImage = function() {
+    this.updateImage = function () {
         if (this.sourceView.image.colorSpace != ColorSpace_Gray) {
             var msgBox = new MessageBox("Source image must be monochrome.", "CubeHelix Error", StdIcon_Error, StdButton_Ok);
             msgBox.execute();
@@ -86,7 +98,7 @@ function CubeHelixColourData()
         P.executeOn(this.preview.mainView);
         this.preview.mainView.endProcess();
 
-        var angle = 2 * Math.PI * (this.startColour / 3.0 + 1 + this.rotations*this.rotdir);
+        var angle = 2 * Math.PI * (this.startColour / 3.0 + 1 + this.rotations * this.rotdir);
 
         var P = new PixelMath;
         P.expression = "$T^" + this.gamma + " + (" + this.saturation + " * $T^" + this.gamma + " * (1 - $T) / 2.0) * (-0.14861 * cos(" + angle + " * $T) + 1.78277 * sin(" + angle + " * $T))";
@@ -121,7 +133,7 @@ function CubeHelixColourData()
         console.hide();
     }
 
-    this.cubeHelix = function(fract, start, rotns, saturation, gamma, irgb) {
+    this.cubeHelix = function (fract, start, rotns, saturation, gamma, irgb) {
         var angle = 2 * Math.PI * (start / 3.0 + 1 + rotns * fract);
         var f = Math.pow(fract, gamma);
         var amp = saturation * f * (1 - fract) / 2.0;
@@ -141,17 +153,16 @@ function CubeHelixColourData()
         }
     }
 
-    this.updataParams = function(value) {
+    this.updataParams = function (value) {
         console.writeln("Updating parameters... ", format("%1.2f", value));
     }
 }
 
-function MyDialog()
-{
+function MyDialog() {
     this.__base__ = Dialog;
     this.__base__();
 
-    this.onHide = function() {
+    this.onHide = function () {
         // data.preview.forceClose();
     }
 
@@ -163,14 +174,14 @@ function MyDialog()
     this.titleLabel.wordWrapping = true;
     this.titleLabel.useRichText = true;
     this.titleLabel.text = "<b>CubeHelix v" + VERSION + "</b>" +
-                           "<p>This script is an adaptation of the cubehelix pseudo " +
-                           "colour mapping scheme developed by Dave Green of the " +
-                           "University of Cambridge.  The colour scheme was developed " +
-                           "specifically for astronomical use and is described in his paper:</p>" +
-                           "<p>&ldquo;Green, D. A., 2011, `A colour scheme for the display of astronomical " +
-                           "intensity images', Bulletin of the Astronomical Society of India, 39, 289&rdquo; " +
-                           "(https://astron-soc.in/bulletin/11June/289392011.pdf).</p>" +
-                           "<p>Copyright &copy; Kevin Osborn 2021</p>";
+        "<p>This script is an adaptation of the cubehelix pseudo " +
+        "colour mapping scheme developed by Dave Green of the " +
+        "University of Cambridge.  The colour scheme was developed " +
+        "specifically for astronomical use and is described in his paper:</p>" +
+        "<p>&ldquo;Green, D. A., 2011, `A colour scheme for the display of astronomical " +
+        "intensity images', Bulletin of the Astronomical Society of India, 39, 289&rdquo; " +
+        "(https://astron-soc.in/bulletin/11June/289392011.pdf).</p>" +
+        "<p>Copyright &copy; Kevin Osborn 2021</p>";
 
     this.sourceSizer = new HorizontalSizer;
     this.sourceSizer.spacing = 4;
@@ -181,8 +192,8 @@ function MyDialog()
     this.sourceImageViewList.minWidth = 360;
     this.sourceImageViewList.getAll();
     this.sourceImageViewList.toolTip = "Select source image.";
-    this.sourceImageViewList.onViewSelected = function(view) {
-        data.sourceView = view;
+    this.sourceImageViewList.onViewSelected = function (view) {
+        data.initPreview(view);
     }
 
     this.sourceSizer.add(this.sourceImageLabel);
@@ -201,7 +212,7 @@ function MyDialog()
     this.startColour.setPrecision(2);
     this.startColour.setValue(0.5);
     this.startColour.toolTip = "Starting colour (1.0 red, 2.0 green, 3.0 blue)."
-    this.startColour.onValueUpdated = function(value) {
+    this.startColour.onValueUpdated = function (value) {
         //data.updataParams(value);
         data.startColour = value;
     }
@@ -215,7 +226,7 @@ function MyDialog()
     this.rotations.setRange(0.0, 2.0);
     this.rotations.setPrecision(2);
     this.rotations.setValue(1.0);
-    this.rotations.onValueUpdated = function(value) {
+    this.rotations.onValueUpdated = function (value) {
         //data.updataParams(value);
         data.rotations = value;
     }
@@ -229,7 +240,7 @@ function MyDialog()
     this.saturationParam.setRange(0.0, 2.0);
     this.saturationParam.setPrecision(2);
     this.saturationParam.setValue(1.0);
-    this.saturationParam.onValueUpdated = function(value) {
+    this.saturationParam.onValueUpdated = function (value) {
         //data.updataParams(value);
         data.saturation = value;
     }
@@ -243,7 +254,7 @@ function MyDialog()
     this.gammaParam.setRange(0.2, 1.6);
     this.gammaParam.setPrecision(2);
     this.gammaParam.setValue(1.0);
-    this.gammaParam.onValueUpdated = function(value) {
+    this.gammaParam.onValueUpdated = function (value) {
         //data.updataParams(value);
         data.gamma = value;
     }
@@ -258,13 +269,13 @@ function MyDialog()
     this.posRadioButton = new RadioButton(this);
     this.posRadioButton.text = "Positive";
     this.posRadioButton.checked = true;
-    this.posRadioButton.onCheck = function(checked) {
+    this.posRadioButton.onCheck = function (checked) {
         if (checked)
             data.rotdir = 1;
     }
     this.negRadioButton = new RadioButton(this);
     this.negRadioButton.text = "Negative";
-    this.negRadioButton.onCheck = function(checked) {
+    this.negRadioButton.onCheck = function (checked) {
         if (checked)
             data.rotdir = -1;
     }
@@ -278,16 +289,16 @@ function MyDialog()
 
     this.previewColourMap.setFixedSize(CMAP_WIDTH, CMAP_HEIGHT);
     data.colourScale = this.previewColourMap;
-    this.previewColourMap.onPaint = function() {
+    this.previewColourMap.onPaint = function () {
         var gfx = new Graphics(this);
         for (var i = 0; i < CMAP_WIDTH; i++) {
-            var fract = i/(CMAP_WIDTH-1);
-            var r = data.cubeHelix(fract, data.startColour, data.rotations*data.rotdir, data.saturation, data.gamma, RED);
-            var g = data.cubeHelix(fract, data.startColour, data.rotations*data.rotdir, data.saturation, data.gamma, GREEN);
-            var b = data.cubeHelix(fract, data.startColour, data.rotations*data.rotdir, data.saturation, data.gamma, BLUE);
-            var red = Math.max(Math.min(Math.floor(255*r), 255), 0);
-            var green = Math.max(Math.min(Math.floor(255*g), 255), 0);
-            var blue = Math.max(Math.min(Math.floor(255*b), 255), 0);
+            var fract = i / (CMAP_WIDTH - 1);
+            var r = data.cubeHelix(fract, data.startColour, data.rotations * data.rotdir, data.saturation, data.gamma, RED);
+            var g = data.cubeHelix(fract, data.startColour, data.rotations * data.rotdir, data.saturation, data.gamma, GREEN);
+            var b = data.cubeHelix(fract, data.startColour, data.rotations * data.rotdir, data.saturation, data.gamma, BLUE);
+            var red = Math.max(Math.min(Math.floor(255 * r), 255), 0);
+            var green = Math.max(Math.min(Math.floor(255 * g), 255), 0);
+            var blue = Math.max(Math.min(Math.floor(255 * b), 255), 0);
 
             var rgb = 0xff000000 + (red << 16) + (green << 8) + blue;
             gfx.pen = new Pen(rgb);
@@ -306,25 +317,25 @@ function MyDialog()
     this.applyButton = new PushButton(this);
     this.applyButton.text = "Apply";
     this.applyButton.icon = this.scaledResource(":/icons/gears.png");
-    this.applyButton.onClick = function() {
+    this.applyButton.onClick = function () {
         data.updateImage();
     }
 
     this.okButton = new PushButton(this);
     this.okButton.text = "OK";
     this.okButton.icon = this.scaledResource(":/icons/ok.png");
-    this.okButton.onClick = function() {
+    this.okButton.onClick = function () {
         this.dialog.ok();
     }
 
     this.cancelButton = new PushButton(this);
     this.cancelButton.text = "Cancel";
     this.cancelButton.icon = this.scaledResource(":/icons/cancel.png");
-    this.cancelButton.onClick = function() {
+    this.cancelButton.onClick = function () {
         data.preview.forceClose();
         this.dialog.cancel();
     }
-    
+
     this.buttonSizer = new HorizontalSizer(this);
     this.buttonSizer.margin = 6;
     this.buttonSizer.spacing = 4;
@@ -348,10 +359,9 @@ function MyDialog()
     this.setFixedSize();
 }
 
-function main()
-{
+function main() {
     var dlg = new MyDialog;
-    dlg.execute();    
+    dlg.execute();
 }
 
 MyDialog.prototype = new Dialog;
